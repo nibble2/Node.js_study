da248b3f96a19c54303eb876b9ed5e5773634c9d
nibble2 <nibblekk@gmail.com>
2019-05-07 17:21:41 +0900

    cli program

---
+++ mongo/routes/cmments.js
@@ -0,0 +1,20 @@
+const express = require('express') ;
+const router = express.Router() ;
+
+router.get('/:id', (req, res, next) => {
+
+}) ;
+
+router.patch('/:id', (req, res, next) => {
+
+});
+
+router.delete('/:id', (req, res, next) => {
+
+}) ;
+
+router.post('/', (req, res, next) => {
+
+}) ;
+
+module.exports = router ;
